d4a39edd8445f239ddad8552ca9d9c13687f0535
RK <rkfrmda3@gmail.com>
2025-04-16 15:36:55 -0500

    Added Config.Debug

--- config.lua
+++ config.lua
@@ -2,8 +2,10 @@
 --- @field afkTimeMinutes number Minutes of inactivity before CAPTCHA appears
 --- @field kickTime number Seconds to complete CAPTCHA before kick
 --- @field captchaLength number Length of the CAPTCHA string
+--- @field debug boolean Whether to show debug messages
 Config = {
-    afkTimeMinutes = 30,
+    afkTimeMinutes = 1,
     kickTime = 60,
-    captchaLength = 6
+    captchaLength = 6,
+    debug = false
 }
